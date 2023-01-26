ef15cc7dec60c3fa9fe0d9b511460dc207fefc74
Gregory Peters <gregorypeters@Gregorys-MBP.attlocal.net>
2023-01-25 23:18:24 -0800

    Finishing Product Page

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,9 +1,19 @@
 import Layout from "../components/Layout";
+import { useContext, useEffect, useState } from "react";
+import { ProductsContext } from "../components/ProductsContext";
 
 export default function CheckoutPage () {
+    const {selectedProducts} = useContext(ProductsContext)
+    const [productsInfo, setProductsInfo] = useState([])
+    useEffect(() => {
+        const uniqIds = [...new Set(selectedProducts)]
+        fetch('/api/products?ids='+uniqIds.join(','))
+        .then(response => response.json())
+        .then(json => setProductsInfo(json) )
+    }, [selectedProducts])
     return (
         <Layout>
-            checkout 
+            {selectedProducts.join(',')}
         </Layout>
     )
 }
