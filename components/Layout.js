f55d7893ec90ce1876b0af4c93870f5cae749b4b
Gregory Peters <gregorypeters@Gregorys-MBP.attlocal.net>
2023-01-30 15:48:54 -0800

    successful stripe pmt

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Footer from "./Footer";
+import { ProductsContext } from "./ProductsContext";
 
 function Layout({ children }) {
+  const { setSelectedProducts } = useContext(ProductsContext);
+  const [success, setSuccess] = useState(false)
+  useEffect(() => {
+    if (window.location.href.includes("success")) {
+      setSelectedProducts([]);
+      setSuccess(true)
+    }
+  }, []);
   return (
     <div>
-      <div className="p-5">{children}</div>
+      <div className="p-5">
+      {success && (
+        <div className="mb-5 bg-green-400 text-white text-lg p-5 rounded-xl">
+        Thanks for your order!
+        </div>
+      )}
+      {children}
+      </div>
       <Footer />
     </div>
   );
